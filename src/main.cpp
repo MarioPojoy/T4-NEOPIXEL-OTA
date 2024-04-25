bb4fa2a9fb4de7c2ffb4d42406cec00fef7ae115
MarioPojoy <mariopc18@gmail.com>
2024-04-25 17:39:07 -0600

    Updated code

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,27 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 #include <Adafruit_NeoPixel.h>
 #include "credentials.h"
 
-#define LED_PIN   D2
-#define LED_COUNT 16
+#define LED_PIN    GPIO_NUM_16
+#define LED_COUNT  16
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 unsigned long pixelPrevious = 0;     
 unsigned long patternPrevious = 0;     
 int           patternCurrent = 0;      
-int           patternInterval = 5000;  
+int           patternInterval = 500;  
 int           pixelInterval = 10;      
 int           pixelQueue = 0;           
 int           pixelCycle = 0;           
 uint16_t      pixelCurrent = 0;         
 uint16_t      pixelNumber = LED_COUNT;  
-const char* hostname = "esp8266-neopixel";
+
+const char* hostname = "esp32-neopixel";
 
 void colorWipe(uint32_t color, int wait) {
   if(pixelInterval != wait)
@@ -32,12 +33,12 @@ void colorWipe(uint32_t color, int wait) {
     pixelCurrent = 0;
 }
 
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
     Serial.println("Connection Failed! Rebooting...");
     delay(5000);
@@ -56,20 +57,19 @@ void setup() {
   ArduinoOTA
     .onStart([]() {
       String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
+      if (ArduinoOTA.getCommand() == U_FLASH)
         type = "sketch";
-      } else {
+      else
         type = "filesystem";
-      }
       Serial.println("Start updating " + type);
-    });
-    ArduinoOTA.onEnd([]() {
+    })
+    .onEnd([]() {
       Serial.println("\nEnd");
-    });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
       Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) {
+    })
+    .onError([](ota_error_t error) {
       Serial.printf("Error[%u]: ", error);
       if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
       else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
@@ -77,18 +77,19 @@ void setup() {
       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
       else if (error == OTA_END_ERROR) Serial.println("End Failed");
     });
-  
   ArduinoOTA.begin();
+
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
   strip.begin();      
   strip.show(); 
   strip.setBrightness(10);
 }
 
 void loop() {
-ArduinoOTA.handle();
+  ArduinoOTA.handle();
   unsigned long currentMillis = millis();                  
   if((currentMillis - patternPrevious) >= patternInterval) { 
     patternPrevious = currentMillis;
